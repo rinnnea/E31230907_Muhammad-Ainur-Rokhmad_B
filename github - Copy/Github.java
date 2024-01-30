8c4c00b8817ee42dfe43f085ceb9a1af7b817624
rinnnea <ainurmuhammad70@gmail.com>
2024-01-30 15:41:53 +0700

    Create Github.java

---
+++ github - Copy/Github.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github;
+
+/**
+ *
+ * @author muham
+ */
+public class Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+    
+}
